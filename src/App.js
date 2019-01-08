1e795f28ff85fe967b2d8723339dd47d8010e908
matt_moe <mrobertmoe@gmail.com>
2019-01-08 14:39:29 -0800

    Create React Javascript template

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import SomeComponent from './SomeComponent.js';
+import SomeNavbar from './SomeNavbar';
+import './Stylesheets/App.css';
+import './Stylesheets/SomeNavbar.css';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header className="navbar-box">
+        <SomeNavbar />
         </header>
+
+        <SomeComponent />
+       
+
       </div>
     );
   }
---
+++ src/SomeComponent.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class SomeComponent extends Component {
+
+  render() {
+    return (
+      <div> Second Attempt! </div>
+    )
+  }
+};
+
+export default SomeComponent;
