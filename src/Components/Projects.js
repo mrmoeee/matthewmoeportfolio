245ffd499977286bdfcf6d6b4ed427fa4be82f39
matt_moe <mrobertmoe@gmail.com>
2019-01-14 16:41:23 -0800

    project name place holders added

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -7,14 +7,17 @@ class Projects extends Component{
       <div className='body-box'> 
         <ul>
           <li>
+            <div>Project Name</div>
             <div>Project Image</div>
             <div>Project Description</div>
           </li>
           <li>
+            <div>Project 2 Name</div>
             <div>Project 2 Image</div>
             <div>Project 2 Description</div>
           </li>
           <li>
+            <div>Project 3 Name</div>
             <div>Project 3 image</div>
             <div>Project 3 Description</div>
           </li>
